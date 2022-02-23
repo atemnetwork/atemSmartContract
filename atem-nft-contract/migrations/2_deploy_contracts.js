402ca4ab16250a34633925fba4824f7cfe45face
SolanaEngineer <>
2022-02-23 10:36:14 +0300

    finish/atem passport contract, need to be tested

--- atem-nft-contract/migrations/2_deploy_contracts.js
+++ atem-nft-contract/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-const SnowGlobe = artifacts.require("SnowGlobe");
+const AtemPassport = artifacts.require("AtemPassport");
 const baseURL = "https://gateway.pinata.cloud/ipfs/QmR8cPsZCxJxj9bVtPfdhWg6xpuaqpEHRXMi82y5uhjesx/"
 
 module.exports = function(deployer) {
-  deployer.deploy(SnowGlobe, baseURL);
+  deployer.deploy(AtemPassport, baseURL);
 };
